a7cdc58c8039229e57bcdecd0b000f580b354897
William Ehlhardt <williamehlhardt@gmail.com>
2021-01-27 23:36:48 -0800

    try a go server

---
+++ server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func createMarketHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("createMarketHandler:", err)
+		return
+	}
+
+	log.Println("lol creating market? ", data)
+}
+
+func main() {
+	staticFiles := http.FileServer(http.Dir("./elm/dist"))
+	http.Handle("/static", staticFiles)
+	http.HandleFunc("/api/create_market", createMarketHandler)
+
+	log.Println("got a server yo: http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
