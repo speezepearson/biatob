18b9fe7bf20e641c62c9082946e7bc25838a7124
Spencer Pearson <speeze.pearson+git@gmail.com>
2021-02-14 00:19:57 -0700

    add "who do you trust" widget to Settings page

--- README.markdown
+++ README.markdown
@@ -33,7 +33,7 @@ TODO
 
     - rename "My Predictions" to be more like... I dunno, "My Stakes"? And add filters for "mine"/"not mine"/"closed"/...
     - write copy explaining how to operationalize a bet well (or find somebody else's explanation)
-    - add /settings
+    - add "okay if empty but untouched" flag to Field to reduce visual noise
 
 - **Explanations:**
 
--- elm/src/TrustedUsersWidget.elm
+++ elm/src/TrustedUsersWidget.elm
@@ -1,4 +1,4 @@
-module TrustedUsersWidget exposing (..)
+port module TrustedUsersWidget exposing (..)
 
 import Browser
 import Html as H exposing (Html)
@@ -13,19 +13,25 @@ import Utils
 
 import Field exposing (Field)
 
+port changed : () -> Cmd msg
+
 type alias Model =
   { trustedUsers : List Pb.UserId
+  , addTrustedUserField : Field () String
   , working : Bool
   , notification : Html Msg
   }
 
 type Msg
   = RemoveTrust Pb.UserId
+  | SetAddTrustedField String
+  | AddTrusted
   | SetTrustedFinished (Result Http.Error Pb.SetTrustedResponse)
 
 init : List Pb.UserId -> ( Model , Cmd Msg )
 init trustedUsers =
   ( { trustedUsers = trustedUsers
+    , addTrustedUserField = Field.init "" <| \() s -> if String.isEmpty s then Err "must not be empty" else Ok s
     , working = False
     , notification = H.text ""
     }
@@ -42,10 +48,24 @@ postSetTrusted req =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    SetAddTrustedField s ->
+      ( { model | addTrustedUserField = model.addTrustedUserField |> Field.setStr s }
+      , Cmd.none
+      )
+    AddTrusted ->
+      case Field.parse () model.addTrustedUserField of
+        Ok username ->
+          ( { model | addTrustedUserField = model.addTrustedUserField |> Field.setStr ""
+                    , working = True
+                    , notification = H.text ""
+            }
+          , postSetTrusted {who=Just {kind=Just (Pb.KindUsername username)}, trusted=True}
+          )
+        Err _ ->
+          ( model , Cmd.none )
     RemoveTrust victim ->
       if List.member victim model.trustedUsers then
-        ( { model | trustedUsers = model.trustedUsers |> List.filter ((/=) victim)
-                  , working = True
+        ( { model | working = True
                   , notification = H.text ""
           }
         , postSetTrusted {who=Just victim, trusted=False}
@@ -59,7 +79,7 @@ update msg model =
     SetTrustedFinished (Ok resp) ->
       case resp.setTrustedResult of
         Just (Pb.SetTrustedResultOk _) ->
-          ( { model | working = False } , Cmd.none )
+          ( { model | working = False } , changed () )
         Just (Pb.SetTrustedResultError e) ->
           ( { model | working = False , notification = Utils.redText (Debug.toString e) }
           , Cmd.none
@@ -71,12 +91,30 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  if List.isEmpty model.trustedUsers then
-    H.text "You don't trust anybody yet!"
-  else
-    H.ul []
-    <| List.map (\u -> H.li [] [Utils.renderUser u, H.text " ", H.button [HE.onClick (RemoveTrust u)] [H.text "Remove trust"]])
-    <| model.trustedUsers
+  H.div []
+    [ model.notification
+    , if List.isEmpty model.trustedUsers then
+        H.text "You don't trust anybody yet!"
+      else
+        H.ul []
+        <| List.map (\u -> H.li []
+            [ Utils.renderUser u
+            , H.text " "
+            , H.button
+                [ HE.onClick (RemoveTrust u)
+                , HA.disabled model.working
+                ] [H.text "Remove trust"]
+            ])
+        <| model.trustedUsers
+    , H.br [] []
+    , H.text "Or: "
+    , Field.inputFor SetAddTrustedField () model.addTrustedUserField
+        H.input
+        [ HA.disabled model.working
+        , HA.placeholder "username"
+        ] []
+    , H.button [HE.onClick AddTrusted] [H.text "Add trust"]
+    ]
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.none
