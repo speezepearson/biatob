c711353071f709667144ca1d15d8ec1c3001b58f
Spencer Pearson <speeze.pearson+git@gmail.com>
2021-02-14 21:33:33 -0700

    change nfsdeploy dest to /home/protected/src to avoid monopolizing the directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file assumes that:
-# - you've rsynced biatob into /home/protected/ on a NearlyFreeSpeech server, probably using `doit nfsdeploy --nfsuser=MYNFSUSER`;
+# - you've rsynced biatob into /home/protected/src/ on a NearlyFreeSpeech server, probably using `doit nfsdeploy --nfsuser=MYNFSUSER`;
 # - that's where this file is;
 # - it's being run by a NearlyFreeSpeech daemon, with cwd /home/protected/
 # - you've configured a proxy on your instance to redirect / to :8080/
@@ -11,9 +11,15 @@ echo '########################################'
 date -Iseconds
 
 if [ ! -d venv ]; then
-  python3 -m venv ./venv
-  ./venv/bin/pip install -r server/requirements.txt
+  python3 -m venv /home/protected/venv
 fi
-source ./venv/bin/activate
+source /home/protected/venv/bin/activate
 
-python -m server.server --host=0.0.0.0 --port=8080 --state-path=/home/protected/server.WorldState.pb
+pip install -r server/requirements.txt
+python -m server.server \
+  --host=0.0.0.0 \
+  --port=8080 \
+  --state-path=/home/protected/server.WorldState.pb \
+  --credentials-path=/home/protected/server.CredentialsConfig.textproto
+  "$@"
+# e.g. --email-daily-backups-to=somebody@example.com
