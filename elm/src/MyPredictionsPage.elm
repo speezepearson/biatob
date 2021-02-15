6c3d995ec378230d7f44f0dd64b227c4d9a6d80c
Spencer Pearson <speeze.pearson+git@gmail.com>
2021-02-14 23:04:12 -0700

    sort and filter My Predictions

--- elm/src/MyPredictionsPage.elm
+++ elm/src/MyPredictionsPage.elm
@@ -3,6 +3,7 @@ module MyPredictionsPage exposing (..)
 import Browser
 import Html as H exposing (Html)
 import Html.Attributes as HA
+import Html.Events as HE
 import Json.Decode as JD
 import Dict exposing (Dict)
 import Time
@@ -12,20 +13,134 @@ import Utils
 
 import Biatob.Proto.Mvp exposing (StakeResult(..))
 import ViewPredictionPage
+import Task
+
+type LifecyclePhase
+  = Open
+  | NeedsResolution
+  | Resolved
+phaseMatches : Time.Posix -> LifecyclePhase -> Pb.UserPredictionView -> Bool
+phaseMatches now phase prediction =
+  case phase of
+    Open ->
+      Utils.currentResolution prediction == Pb.ResolutionNoneYet
+      && prediction.closesUnixtime > Utils.timeToUnixtime now
+      && prediction.resolvesAtUnixtime > Utils.timeToUnixtime now
+    NeedsResolution ->
+      Utils.currentResolution prediction == Pb.ResolutionNoneYet
+      && prediction.resolvesAtUnixtime < Utils.timeToUnixtime now
+    Resolved ->
+      Utils.currentResolution prediction /= Pb.ResolutionNoneYet
+
+type alias Filter =
+  { own : Maybe Bool
+  , phase : Maybe LifecyclePhase
+  }
+filterMatches : Time.Posix -> Filter -> Pb.UserPredictionView -> Bool
+filterMatches now filter prediction =
+  List.all identity
+    [ filter.own
+      |> Maybe.map ((==) (Utils.mustPredictionCreator prediction).isSelf)
+      |> Maybe.withDefault True
+    , filter.phase
+      |> Maybe.map (\phase -> phaseMatches now phase prediction)
+      |> Maybe.withDefault True
+    ]
+viewFilterInput : Filter -> Html Msg
+viewFilterInput filter =
+  H.span []
+    [ H.select
+        [ HE.onInput <| \s -> case s of
+            "all owners" -> SetFilter {filter | own = Nothing}
+            "my own" -> SetFilter {filter | own = Just True}
+            "not mine" -> SetFilter {filter | own = Just False}
+            _ -> Debug.todo <| "unrecognized value: " ++ s
+        , HA.value <| case filter.own of
+            Nothing -> "all owners"
+            Just True -> "my own"
+            Just False -> "not mine"
+        ]
+        [ H.option [HA.value "all owners"] [H.text "all owners"]
+        , H.option [HA.value "my own"] [H.text "my own"]
+        , H.option [HA.value "not mine"] [H.text "not mine"]
+        ]
+    , H.select
+        [ HE.onInput <| \s -> case s of
+            "all phases" -> SetFilter {filter | phase = Nothing}
+            "open" -> SetFilter {filter | phase = Just Open}
+            "needs resolution" -> SetFilter {filter | phase = Just NeedsResolution}
+            "resolved" -> SetFilter {filter | phase = Just Resolved}
+            _ -> Debug.todo <| "unrecognized value: " ++ s
+        , HA.value <| case filter.phase of
+            Nothing -> "all phases"
+            Just Open -> "open"
+            Just NeedsResolution -> "needs resolution"
+            Just Resolved -> "resolved"
+        ]
+        [ H.option [HA.value "all phases"] [H.text "all phases"]
+        , H.option [HA.value "open"] [H.text "open"]
+        , H.option [HA.value "needs resolution"] [H.text "needs resolution"]
+        , H.option [HA.value "resolved"] [H.text "resolved"]
+        ]
+    ]
+
+type Ordering = Asc | Desc
+sortKeySign : Ordering -> number
+sortKeySign dir =
+  case dir of
+    Asc -> 1
+    Desc -> -1
+type SortOrder
+  = ResolutionDate Ordering
+  | CreatedDate Ordering
+viewSortOrderInput : SortOrder -> Html Msg
+viewSortOrderInput order =
+  H.select
+    [ HE.onInput <| \s -> case s of
+        "created, desc" -> SetSortOrder (CreatedDate Desc)
+        "created, asc" -> SetSortOrder (CreatedDate Asc)
+        "resolves, desc" -> SetSortOrder (ResolutionDate Asc)
+        "resolves, asc" -> SetSortOrder (ResolutionDate Asc)
+        _ -> Debug.todo <| "unrecognized value: " ++ s
+    , HA.value <| case order of
+        CreatedDate Desc -> "created, desc"
+        CreatedDate Asc -> "created, asc"
+        ResolutionDate Desc -> "resolves, desc"
+        ResolutionDate Asc -> "resolves, asc"
+    ]
+    [ H.option [HA.value "created, desc"] [H.text "created, desc"]
+    , H.option [HA.value "created, asc"] [H.text "created, asc"]
+    , H.option [HA.value "resolves, desc"] [H.text "resolves, desc"]
+    , H.option [HA.value "resolves, asc"] [H.text "resolves, asc"]
+    ]
+
+sortPredictions : (a -> Pb.UserPredictionView) -> SortOrder -> List a -> List a
+sortPredictions toPrediction order predictions =
+  case order of
+    ResolutionDate dir ->
+      List.sortBy (toPrediction >> \p -> p.resolvesAtUnixtime * sortKeySign dir) predictions
+    CreatedDate dir ->
+      List.sortBy (toPrediction >> \p -> p.createdUnixtime * sortKeySign dir) predictions
 
 type alias Model =
   { predictions : Dict Int ViewPredictionPage.Model
-  , auth : Maybe Pb.AuthToken
+  , filter : Filter
+  , order : SortOrder
+  , auth : Pb.AuthToken
+  , now : Time.Posix
   }
 
 type Msg
   = PredictionPageMsg Int ViewPredictionPage.Msg
+  | Tick Time.Posix
+  | SetSortOrder SortOrder
+  | SetFilter Filter
 
 init : JD.Value -> (Model, Cmd Msg)
 init flags =
   let
-    auth : Maybe Pb.AuthToken
-    auth =  Utils.decodePbFromFlags Pb.authTokenDecoder "authTokenPbB64" flags
+    auth : Pb.AuthToken
+    auth =  Utils.mustDecodePbFromFlags Pb.authTokenDecoder "authTokenPbB64" flags
     predictions : Dict Int Pb.UserPredictionView
     predictions = Utils.mustDecodePbFromFlags Pb.predictionsByIdDecoder "predictionsPbB64" flags |> Utils.mustPredictionsById
 
@@ -33,15 +148,20 @@ init flags =
     subinits =
       Dict.map
         (\id m ->
-          let (submodel, subcmd) = ViewPredictionPage.initBase {predictionId=id, prediction=m, auth=auth, now=Time.millisToPosix 0} in
+          let (submodel, subcmd) = ViewPredictionPage.initBase {predictionId=id, prediction=m, auth=Just auth, now=Time.millisToPosix 0} in
           (submodel, subcmd)
         )
         predictions
   in
   ( { predictions = Dict.map (\_ (submodel, _) -> submodel) subinits
+    , filter = { own = Nothing , phase = Nothing }
+    , order = CreatedDate Desc
     , auth = auth
+    , now = Time.millisToPosix 0
     }
-  , Cmd.batch <| List.map (\(id, (_, subcmd)) -> Cmd.map (PredictionPageMsg id) subcmd) <| Dict.toList subinits
+  , Cmd.batch
+    <| (::) (Task.perform Tick Time.now)
+    <| List.map (\(id, (_, subcmd)) -> Cmd.map (PredictionPageMsg id) subcmd) <| Dict.toList subinits
   )
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -55,15 +175,32 @@ update msg model =
           ( { model | predictions = model.predictions |> Dict.insert predictionId newPredictionPage }
           , Cmd.map (PredictionPageMsg predictionId) predictionPageCmd
           )
+    Tick t ->
+      ( { model | now = t }
+      , Cmd.none
+      )
+
+    SetSortOrder order ->
+      ( { model | order = order }
+      , Cmd.none
+      )
+
+    SetFilter filter ->
+      ( { model | filter = filter }
+      , Cmd.none
+      )
 
 
 view : Model -> Html Msg
 view model =
   H.div []
     [ H.h2 [] [H.text "My Predictions"]
-    , if model.auth == Nothing then
-        H.text "You're not logged in, so I don't know what predictions to show you!"
-      else if Dict.isEmpty model.predictions then
+    , H.text "Filter: "
+    , viewFilterInput model.filter
+    , H.text " Sort: "
+    , viewSortOrderInput model.order
+    , H.hr [] []
+    , if Dict.isEmpty model.predictions then
         H.div []
           [ H.text "You haven't participated in any predictions yet!"
           , H.br [] []
@@ -74,7 +211,8 @@ view model =
       else
         model.predictions
         |> Dict.toList
-        |> List.sortBy (\(id, _) -> id)
+        |> sortPredictions (\(_, m) -> m.prediction) model.order
+        |> List.filter (\(_, m) -> filterMatches model.now model.filter m.prediction)
         |> List.map (\(id, m) -> H.div [HA.style "margin" "1em", HA.style "padding" "1em", HA.style "border" "1px solid black"] [ViewPredictionPage.view m |> H.map (PredictionPageMsg id)])
         |> List.intersperse (H.hr [] [])
         |> H.div []
