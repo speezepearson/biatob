dd1cf78eb1a78f7c94b09a9eb89f09dde1df27cc
Spencer Pearson <speeze.pearson+git@gmail.com>
2021-03-01 09:43:24 -0700

    make a real Login page, to get the navbar at the top

---
+++ elm/src/Elements/Login.elm
@@ -0,0 +1,23 @@
+module Elements.Login exposing (main)
+
+import Html as H
+import Json.Decode as JD
+
+import Biatob.Proto.Mvp as Pb
+import Utils
+
+import Page
+import Page.Program
+
+type alias Model = ()
+type alias Msg = Never
+
+pagedef : Page.Element Model Msg
+pagedef =
+  { init = \_ -> ((), Page.NoCmd)
+  , view = \g m -> {title="Log in", body=[H.h2 [] [H.text "Log in"], H.main_ [] [H.text "...using the navbar at the top, as always."]]}
+  , update = never
+  , subscriptions = \() -> Sub.none
+  }
+
+main = Page.Program.page pagedef
