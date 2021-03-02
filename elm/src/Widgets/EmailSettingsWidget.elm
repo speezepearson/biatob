2f231b45b01540acedd42a99b97b24fba53030a2
Spencer Pearson <speeze.pearson+git@gmail.com>
2021-03-01 22:10:00 -0700

    [ui] always display email option checkboxes, just disable them when not verified

--- elm/src/Widgets/EmailSettingsWidget.elm
+++ elm/src/Widgets/EmailSettingsWidget.elm
@@ -118,61 +118,76 @@ view globals model =
   case Page.getUserInfo globals of
     Nothing -> H.text "(Log in to view your email settings!)"
     Just userInfo ->
-      case userInfo |> Utils.mustUserInfoEmail |> Utils.mustEmailFlowStateKind of
-        Pb.EmailFlowStateKindUnstarted _ ->
-          H.div []
-            [ H.text "Register an email address for notifications: "
-            , Field.inputFor SetEmailField () model.emailField
-                H.input
-                [ HA.type_ "email"
-                , HA.disabled <| model.working
-                , HA.placeholder "email@ddre.ss"
-                , Utils.onEnter SetEmail Ignore
-                ] []
-            , H.button
-                [ HE.onClick SetEmail
-                , HA.disabled <| model.working || Result.toMaybe (Field.parse () model.emailField) == Nothing
-                ] [H.text "Send verification"]
-            , model.notification |> H.map never
-            ]
-        Pb.EmailFlowStateKindCodeSent {email} ->
-          H.div []
-            [ H.text "I sent a verification code to "
-            , Utils.b email
-            , H.text ". Enter it here: "
-            , Field.inputFor SetCodeField () model.codeField
-                H.input
-                [ HA.disabled <| model.working
-                , HA.placeholder "code"
-                , Utils.onEnter VerifyEmail Ignore
-                ] []
-            , H.button
-                [ HE.onClick VerifyEmail
-                , HA.disabled <| model.working || Result.toMaybe (Field.parse () model.codeField) == Nothing
-                ] [H.text "Verify code"]
-              -- TODO: "Resend email"
-            , model.notification |> H.map never
-            ]
-        Pb.EmailFlowStateKindVerified email ->
-          H.div []
-            [ H.text "Your email address is: "
-            , H.strong [] [H.text email]
-            , H.div []
-                [ H.input
-                    [ HA.type_ "checkbox", HA.checked userInfo.emailRemindersToResolve
-                    , HA.disabled model.working
-                    , HE.onInput (\_ -> SetEmailRemindersToResolve (not userInfo.emailRemindersToResolve))
+      let
+        emailFlowState : Pb.EmailFlowStateKind
+        emailFlowState = userInfo |> Utils.mustUserInfoEmail |> Utils.mustEmailFlowStateKind
+
+        isRegistered : Bool
+        isRegistered = case emailFlowState of
+          Pb.EmailFlowStateKindUnstarted _ -> False
+          Pb.EmailFlowStateKindCodeSent _ -> False
+          Pb.EmailFlowStateKindVerified _ -> True
+        registrationBlock : Html Msg
+        registrationBlock =
+          case userInfo |> Utils.mustUserInfoEmail |> Utils.mustEmailFlowStateKind of
+            Pb.EmailFlowStateKindUnstarted _ ->
+              H.div []
+                [ H.text "Register an email address for notifications: "
+                , Field.inputFor SetEmailField () model.emailField
+                    H.input
+                    [ HA.type_ "email"
+                    , HA.disabled <| model.working
+                    , HA.placeholder "email@ddre.ss"
+                    , Utils.onEnter SetEmail Ignore
                     ] []
-                , H.text " Email reminders to resolve your predictions, when it's time?"
+                , H.button
+                    [ HE.onClick SetEmail
+                    , HA.disabled <| model.working || Result.toMaybe (Field.parse () model.emailField) == Nothing
+                    ] [H.text "Send verification"]
+                , model.notification |> H.map never
                 ]
-            , H.div []
-                [ H.input
-                    [ HA.type_ "checkbox", HA.checked userInfo.emailResolutionNotifications
-                    , HA.disabled model.working
-                    , HE.onInput (\_ -> SetEmailResolutionNotifications (not userInfo.emailResolutionNotifications))
+            Pb.EmailFlowStateKindCodeSent {email} ->
+              H.div []
+                [ H.text "I sent a verification code to "
+                , Utils.b email
+                , H.text ". Enter it here: "
+                , Field.inputFor SetCodeField () model.codeField
+                    H.input
+                    [ HA.disabled <| model.working
+                    , HA.placeholder "code"
+                    , Utils.onEnter VerifyEmail Ignore
                     ] []
-                , H.text " Email notifications when predictions you've bet on resolve?"
+                , H.button
+                    [ HE.onClick VerifyEmail
+                    , HA.disabled <| model.working || Result.toMaybe (Field.parse () model.codeField) == Nothing
+                    ] [H.text "Verify code"]
+                  -- TODO: "Resend email"
+                , model.notification |> H.map never
                 ]
-            , H.br [] []
-            , model.notification |> H.map never
-            ]
+            Pb.EmailFlowStateKindVerified email ->
+              H.div []
+                [ H.text "Your email address is: "
+                , H.strong [] [H.text email]
+                , H.br [] []
+                , model.notification |> H.map never
+                ]
+      in
+        H.div []
+          [ registrationBlock
+          , H.div []
+              [ H.input
+                  [ HA.type_ "checkbox", HA.checked userInfo.emailRemindersToResolve
+                  , HA.disabled (model.working || not isRegistered)
+                  , HE.onInput (\_ -> SetEmailRemindersToResolve (not userInfo.emailRemindersToResolve))
+                  ] []
+              , H.text " Email reminders to resolve your predictions, when it's time?"
+              ]
+          , H.div []
+              [ H.input
+                  [ HA.type_ "checkbox", HA.checked userInfo.emailResolutionNotifications
+                  , HA.disabled (model.working || not isRegistered)
+                  , HE.onInput (\_ -> SetEmailResolutionNotifications (not userInfo.emailResolutionNotifications))
+                  ] []
+              , H.text " Email notifications when predictions you've bet on resolve?"
+              ]
+          ]
